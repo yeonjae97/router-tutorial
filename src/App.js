c1162c276dbdaf745fd134efd36a2faaf87544ca
yeonjae <yeonjae978@gmail.com>
2023-05-11 11:21:46 +0900

    2023-05-11 두 번째 커밋

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import {Route, Routes} from 'react-router-dom';
 import About from './pages/About';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
+import Article from './pages/Article';
+import Articles from './pages/Articles';
+import Layout from './Layout';
 
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/about" element={<About />}/>
-      <Route path="/profiles/:username" element={<Profile />}/>
+      <Route element={<Layout />}>
+        <Route index element={<Home />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/profiles/:username" element={<Profile />}/>
+      </Route>
+      <Route path="/articles" element={<Articles />}>
+        <Route path=":id" element={<Article />}/>
+      </Route>
     </Routes>
   );
 };
